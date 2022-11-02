9011b670e98b1e4f830d065f5604cf75f512cf65
Serg5858 <ss3@ro.ru>
2022-11-02 17:03:33 +0300

    Added Allure

--- src/test/java/ru/netology/delivery/data/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/data/DeliveryTest.java
@@ -1,7 +1,7 @@
 package ru.netology.delivery.data;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -10,10 +10,19 @@ import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 
 public class DeliveryTest {
+    @BeforeAll
+    static void setUp(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
     }
+
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
